2393ce2f3005d82606936f9afb09dd81cb35c6de
Valeria Jin <51115379+AndandoJing@users.noreply.github.com>
2026-08-22 20:20:54 +0200

    Create print.c

---
+++ Rush01/print.c
@@ -0,0 +1,30 @@
+void  ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void  ft_error(void)
+{
+  write(1, "Errror\n", 1);
+}
+
+void  ft_print_grid(int grid[4][4])
+{
+  int  row;
+  int  col;
+
+  row = 0;
+  while (row < 4)
+    {
+        col = 0;
+        while (col < 4)
+          {
+            ft_putchar(grid[row][col] + '0');
+            if (col < 3)
+              ft_putchar(' ');
+            col++;
+          }
+    }
+  ft_putchar('\0');
+  row++;
+}
